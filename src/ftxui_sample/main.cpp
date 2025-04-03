a78f0c3bcf4781cdab20928e8b88eff6fa5babdc
Eugeniusz Lewandowski <you@example.com>
2025-04-03 21:20:14 +0100

    Start plotting library

--- src/ftxui_sample/main.cpp
+++ src/ftxui_sample/main.cpp
@@ -4,43 +4,47 @@
 #include <SFML/System/Clock.hpp>
 #include <SFML/System/Vector2.hpp>
 #include <SFML/Window/VideoMode.hpp>
-#include <imgui.h>// necessary for ImGui::*, imgui-SFML.h doesn't include imgui.h
-
 #include <imgui-SFML.h>// for ImGui::SFML::* functions and SFML-specific overloads
+#include <imgui.h>// necessary for ImGui::*, imgui-SFML.h doesn't include imgui.h
 
 
 int main()
 {
 
   sf::Vector2u screenResolution = { 640, 480 };// NOLINT
-  sf::RenderWindow window(sf::VideoMode{ screenResolution }, "ImGui + SFML = <3");
-  const auto frameRate = 60;
-  window.setFramerateLimit(frameRate);
-  if (!ImGui::SFML::Init(window)) { return -1; }
+  sf::RenderWindow Window(sf::VideoMode{ screenResolution }, "ImGui + SFML = <3");
+  const auto FrameRate = 60;
+  Window.setFramerateLimit(FrameRate);
+  if (!ImGui::SFML::Init(Window)) { return -1; }
 
-  sf::CircleShape shape(100.F);
-  shape.setFillColor(sf::Color::Green);
+  sf::CircleShape Shape(100.F);
+  Shape.setFillColor(sf::Color::Green);
+  // NOLINTNEXTLINE
+  ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_DockingEnable | ImGuiConfigFlags_ViewportsEnable;
 
-  sf::Clock deltaClock;
-  while (window.isOpen()) {
-    while (const auto event = window.pollEvent()) {
-      ImGui::SFML::ProcessEvent(window, *event);
 
-      if (event->is<sf::Event::Closed>()) { window.close(); }
+  sf::Clock DeltaClock;
+  while (Window.isOpen()) {
+    while (const auto Event = Window.pollEvent()) {
+      ImGui::SFML::ProcessEvent(Window, *Event);
+
+      if (Event->is<sf::Event::Closed>()) { Window.close(); }
     }
 
-    ImGui::SFML::Update(window, deltaClock.restart());
+    ImGui::SFML::Update(Window, DeltaClock.restart());
 
-    ImGui::ShowDemoWindow();
+    ImGui::Begin("Challenge 1");
 
-    ImGui::Begin("Hello, world!");
-    ImGui::Button("Look at this pretty button");
     ImGui::End();
 
-    window.clear();
-    window.draw(shape);
-    ImGui::SFML::Render(window);
-    window.display();
+    Window.clear();
+    Window.draw(Shape);
+    ImGui::SFML::Render(Window);
+    if ((0 != (ImGuiConfigFlags_ViewportsEnable & static_cast<unsigned int>(ImGui::GetIO().ConfigFlags)))) {
+      ImGui::UpdatePlatformWindows();
+      ImGui::RenderPlatformWindowsDefault();
+    }
+    Window.display();
   }
 
   ImGui::SFML::Shutdown();
